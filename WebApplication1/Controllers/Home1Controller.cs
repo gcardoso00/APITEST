92f1d80130984acde2c70603474ab9d014bc9754
gcardoso00 <gcardoso005@gmail.com>
2024-01-06 20:08:13 -0300

    Teste inicial commit

---
+++ WebApplication1/Controllers/Home1Controller.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class Home1Controller : Controller
+    {
+          public IActionResult Index()
+          {
+              return View();
+          }
+
+
+        /* [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+         public IActionResult Error()
+         {
+             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+         }*/
+
+       /* public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+       */
+    }
+
+
+
+}
